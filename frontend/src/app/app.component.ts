2ed7878d99eb6445a2c49df41bbd181b5c7972b0
Tatjana Putskova <tpuchkova@gmail.com>
2024-08-03 17:41:04 +0300

    edit  component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {GetAvailableTimesComponent} from "./components/get-available-times/get-available-times.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GetAvailableTimesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
